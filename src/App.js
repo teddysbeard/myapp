254612a7b54d8f2b1d0295c112f27c29e42323f4
teddysbeard <d.komarov@ivel.design>
2023-07-21 16:54:46 +0300

    init: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+<script src="http://10.211.1.92:8097"></script>
 function App() {
+  // const  elem = <div>
+  //   <p class='first-paragraph'>Hello</p>
+  //   <p class='second-paragraph'>World</p>
+  //   </div>;
+	// return elem;
+  // return  (
+  //   <div>
+  //     <p class='first-paragraph'>Hello</p>
+  //     <p class='second-paragraph'>World</p>
+  //   </div>);
+  /* ------------------------------------------------------------------- */
+  // В следующем коде автор кода хочет вернуть тег ul
+  // return (
+  //   <ul>
+	// 		<li>text1</li>
+	// 		<li>text2</li>
+	// 		<li>text3</li>
+	// 	</ul>
+  // );
+  
+ /* Вставка значений переменных и констант в JSX */
+  const str = 'String constant'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>{str} additional text</div>
+  )
 }
 
 export default App;
